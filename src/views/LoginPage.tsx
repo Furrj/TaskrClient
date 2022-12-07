9e5e3e798faba4c281e47f287e50384bd7f0472f
FraterSKS <jackson.a.furr@gmail.com>
2022-12-06 21:50:31 -0500

    forms

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+
+//LAYOUTS
+import CardWrapper from "../layouts/CardWrapper";
+
+//TS
+interface IState {
+  username: string;
+  password: string;
+}
+
+const initState: IState = {
+  username: "",
+  password: "",
+};
 
 const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>;
+  const [userInput, setUserInuput] = useState<IState>(initState);
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUserInuput({
+      ...userInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <CardWrapper>
+      <div className="card mt-3 loginCard">
+        <div>Username: </div>
+        <input
+          type="text"
+          name="username"
+          value={userInput.username}
+          onChange={inputHandler}
+          className="form-control"
+        />
+        <br />
+        <div>Password:</div>
+        <input
+          type="text"
+          name="password"
+          value={userInput.password}
+          onChange={inputHandler}
+          className="form-control"
+        />
+        <br />
+      </div>
+    </CardWrapper>
+  );
 };
 
 export default LoginPage;
